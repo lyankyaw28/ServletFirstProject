b8a0112996cb0dad7e1f4a69407bb432aee5558a
lyankyaw1 <lyan002kyaw002@gmail.com>
2019-09-10 16:35:50 +0630

    complete servlet project.

--- src/development/servlet_sending_email/Mailer.java
+++ src/development/servlet_sending_email/Mailer.java
@@ -1,5 +1,7 @@
 package development.servlet_sending_email;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -12,33 +14,41 @@ import javax.mail.internet.MimeMessage;
 
 public class Mailer {
 	public static void send(String to, String subject, String msg) {
-
-		final String user = "panelopi5@gmail.com";// change accordingly
-		final String pass = "shwesinwin";
-
+		final String fromEmail = "compasstaxiinfo@gmail.com";// change accordingly
+		final String emailPassword = "yngwie500";
 		// 1st step) Get the session object
 		Properties props = System.getProperties();
-		props.setProperty("mail.smtp.auth", "true");
-		props.setProperty("mail.smtp.host", "8080");
-
-		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(user, pass);
-			}
-		});
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.user", fromEmail);
+        props.put("mail.smtp.password", emailPassword);
+        props.put("mail.debug", "true");
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, emailPassword);
+            }
+        });
 		// 2nd step)compose message
 		try {
 			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(user));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			message.setSubject(subject);
+            // set message headers
+			message.addHeader("Content-type", "text/HTML; charset=UTF-8");
+			message.addHeader("format", "flowed");
+			message.addHeader("Content-Transfer-Encoding", "8bit");
+			try {
+				message.setFrom(new InternetAddress(fromEmail, "Compass"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+			message.setReplyTo(InternetAddress.parse(fromEmail, false));
+			message.setSubject(subject, "UTF-8");
+			message.setSentDate(new Date());
 			message.setText(msg);
-
-			// 3rd step)send message
-			Transport.send(message);
-
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+            Transport.send(message);
 			System.out.println("Done");
-
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
 		}
--- src/development/wirte_data_to_pdf/ServletPDF.java
+++ src/development/wirte_data_to_pdf/ServletPDF.java
@@ -1,30 +1,78 @@
 package development.wirte_data_to_pdf;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import javafx.scene.shape.MoveTo;
-
 public class ServletPDF extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		final ServletContext servletContext = request.getSession().getServletContext();
+		final File tempDirectory = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+		final String temperotyFilePath = tempDirectory.getAbsolutePath();
+
+		String fileName = "Generate_Report_" + System.currentTimeMillis() + ".pdf";
 		response.setContentType("application/pdf");
-		response.setHeader("Content-disposition", "inline; filename='javatpoint.pdf'");
-		PDF pdf = new PDF(out);
-		Page p1 = new Page(p);
-		p1.add(new MoveTo(p, 200, 700));
-		p1.add(new Text(p, "www.javatpoint.com"));
-		p1.add(new Text(p, "by Sonoo Jaiswal"));
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Cache-Control", "max-age=0");
+		response.setHeader("Content-disposition", "attachment; " + "filename=" + fileName);
 
-		p.add(p1);
-		p.setAuthor("Ian F. Darwin");
+		try {
+			GeneratePDF.createPDF(temperotyFilePath + "\\" + fileName);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			baos = convertPDFToByteArrayOutputStream(temperotyFilePath + "\\" + fileName);
+			OutputStream os = response.getOutputStream();
+			baos.writeTo(os);
+			os.flush();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
 
-		p.writePDF();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	private static ByteArrayOutputStream convertPDFToByteArrayOutputStream(String fileName) {
+		InputStream inputStream = null;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+
+			inputStream = new FileInputStream(fileName);
+
+			byte[] buffer = new byte[1024];
+			baos = new ByteArrayOutputStream();
+
+			int bytesRead;
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				baos.write(buffer, 0, bytesRead);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return baos;
 	}
 }
