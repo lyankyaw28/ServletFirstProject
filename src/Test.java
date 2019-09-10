a52931c01bdc6e453f34c9f0deefcf3534533b2f
lyankyaw1 <lyan002kyaw002@gmail.com>
2019-09-10 16:44:25 +0630

    complete servlet project.

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-import java.util.Calendar;
-import java.util.Date;
-
-public class Test {
-	public static void main(String args[]) {
-		System.out.println(System.getProperty("user.dir"));
-	}
-}
--- src/servlet_request_interface/DisplayNameServlet.java
+++
@@ -1,9 +0,0 @@
-package servlet_request_interface;
-
-import javax.servlet.http.HttpServlet;
-
-public class DisplayNameServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-
-}
